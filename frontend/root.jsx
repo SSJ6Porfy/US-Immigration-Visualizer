f2409eb6ac3e20242c137cc754a9603bd64bbaaa
Porfirio Matias <SSJ6Porfy@hotmail.com>
2017-10-05 08:49:45 -0700

    Floating div now come to front

--- frontend/root.jsx
+++ frontend/root.jsx
@@ -1,53 +1,81 @@
 import React from "react";
 import * as d3 from "d3";
 import fetchData from "./data_actions";
+import { front } from "svg.js";
 
 class Immigration extends React.Component {
   constructor(props) {
     super(props);
     this.countrySpotGenerator = this.countrySpotGenerator.bind(this);
     this.builder = this.builder.bind(this);
-    this.countryInfo = this.countryInfo.bind(this);
+    this.countryIn = this.countryIn.bind(this);
+    this.countryOut = this.countryOut.bind(this);
   }
 
   builder() {
     fetchData().then(res => this.countrySpotGenerator(res));
   }
 
-  countryInfo(country) {
-    console.log(Object.keys(country));
-    console.log(Object.values(country).slice(0, Object.values(country).length - 1));
+  countryIn(country) {
+    d3.selection.prototype.moveToFront = function() {
+      return this.each(function(){
+        this.parentNode.appendChild(this);
+      });
+    };
+
+    country.moveToFront();
+    country.style("visibility","visible");
+  }
+
+
+  countryOut(country) {
+    country.style("visibility","hidden");
   }
 
   countrySpotGenerator(arr) {
+
     let keys = Object.keys(arr).slice(1);
     keys.forEach((key, idx) => {
       let str = arr[key][0]["FIELD21"];
       let [a, b] = str.split(",");
       let x = Number(a.match(/-?\d+/));
       let y = Number(b.match(/-?\d+/));
+
       let svg = d3.select("svg");
 
-      this.countryInfo(arr[key][0]);
+      let forObj = svg.append("foreignObject")
+                      .attr("width", "200px")
+                      .attr("height", "100px")
+                      .attr("x", x - 200)
+                      .attr("y", y - 120)
+                      .attr("visibility", "hidden");
 
-      let textstuff = svg.append("svg:text")
-      .attr("visibility", "hidden")
-      .attr("x", x)
-      .attr("y", y - 10)
-      .text(function() {return key;})
-      .attr("id", `${idx}-text`);
+      let containerDiv = forObj.append("xhtml:div")
+                                .attr("id", "containerDiv")
+                                .attr("visibility", "hidden")
+                                .style("border", "1px solid white")
+                                .style("border-radius", "3px");
 
-      let circle =  svg.append("circle")
-                        .attr("r", 5)
-                        .attr("cx", x)
-                        .attr("cy", y)
-                        .attr("fill", "red")
-                        .on("mouseover", () => {textstuff.style("visibility","visible");})
-                        .on("mouseout", () => {textstuff.style("visibility","hidden");});
+      let countryBody = containerDiv.append("xhtml:body");
 
-      let path = svg.append('path')
-                  .attr('class', 'flowline')
-                  .attr('d', `M${x} ${y} L0 215`);
+      let countryContent = countryBody.append("xhtml:div")
+                                      .attr("id", "country-title")
+                                      .text(() => {return key;})
+                                      .append("xhtml:div")
+                                      .attr("id", "country-body");
+
+          let circle =  svg.append("circle")
+          .attr("r", 5)
+          .attr("cx", x)
+          .attr("cy", y)
+          .attr("fill", "red")
+          .on("mouseover", () => this.countryIn(forObj))
+          .on("mouseout", () => this.countryOut(forObj));
+
+          let path = svg.append('path')
+          .attr('class', 'flowline')
+          .attr('d', `M${x} ${y} L0 215`);
+
 
     });
   }
